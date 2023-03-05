b921515f1db508381fed8809185232ef733f36bb
KeaganMorrison <114590658+KeaganMorrison@users.noreply.github.com>
2023-03-05 16:45:40 -0500

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,9 @@
+public class Item {
+    int Adder;
+    String title;
+
+    Item(int Adder, String title) {
+        this.Adder=Adder;
+        this.title=title;
+    }
+}
---
+++ SuperStore.java
@@ -0,0 +1,9 @@
+public class SuperStore {
+    public static void main(String[] args) throws Exception {
+    Player Carti = new Player(3000, 5000);
+    System.out.println("Welcome to Playboi Carti Simulator. You, Jordan Terrell Carter, need to drop your coveted ℕ𝕒𝕣𝕔𝕚𝕤𝕤𝕚𝕤𝕥 album.");
+   
+   
+    
+    }
+}
